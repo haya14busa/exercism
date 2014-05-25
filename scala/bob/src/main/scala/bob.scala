8404f53cedf9f25327c45f56552986878da6cc59
haya14busa <hayabusa1419@gmail.com>
2014-05-26 00:45:19 +0900

    Refactoring bob in scala

--- scala/bob/src/main/scala/bob.scala
+++ scala/bob/src/main/scala/bob.scala
@@ -2,13 +2,17 @@ class Bob {
   private def hasLower(s: String): Boolean = s.exists(_.isLower)
   private def hasUpper(s: String): Boolean = s.exists(_.isUpper)
 
+  // Do not have lowercase letters and contains uppercase chars at the same time
+  private def isShouting(str: String) = ! hasLower(str) && hasUpper(str)
+  private def isQuestion(str: String) = str.endsWith("?")
+  private def isSilent(str: String)   = str.forall(_.isWhitespace)
+
   def hey(message: String): String = {
     message match {
-      // Do not have lowercase letters and contains uppercase chars at the same time
-      case s if ! hasLower(s) && hasUpper(s) => "Woah, chill out!"
-      case s if s.endsWith("?")              => "Sure."
-      case s if s.forall(_.isWhitespace)     => "Fine. Be that way!"
-      case _                                 => "Whatever."
+      case s if isShouting(s) => "Woah, chill out!"
+      case s if isQuestion(s) => "Sure."
+      case s if isSilent(s)   => "Fine. Be that way!"
+      case _                  => "Whatever."
     }
   }
 }
