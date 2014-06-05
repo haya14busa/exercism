6d0c5a332d1a49f435eda8edd2d780288a5ee6b4
haya14busa <hayabusa1419@gmail.com>
2014-06-06 00:06:39 +0900

    Refactoring anagram in scala

--- scala/anagram/src/main/scala/anagram.scala
+++ scala/anagram/src/main/scala/anagram.scala
@@ -1,25 +1,13 @@
 class Anagram(anagram: String) {
-  val charCnt = new CharCount(anagram.toLowerCase).charCount.toList.sorted
-
   def matches(candidates: Seq[String]): Seq[String] =  {
-    val uniqueCandidates = candidates.toSet.toList
-    uniqueCandidates.toSet.toList.filter{
-      (x: String) => (new CharCount(x.toLowerCase).charCount.toList.sorted == charCnt)
-    }.filter {
-      // Filter identcal words with triating case insensitive
-      word => word.toLowerCase != anagram.toLowerCase
-    }
-
+    candidates.filter( word => isAnagram(word) && ! isIdentical(word) )
   }
-}
 
-// Helper
-class CharCount(word: String) {
-  val chars   = word.toList
-  val charSet = word.toSet
-  val charCnt = for {
-    char <- charSet.toSeq
-  } yield chars.count(_ == char)
+  private def isAnagram(word: String) = {
+    word.toLowerCase.sorted == anagram.toLowerCase.sorted
+  }
 
-  val charCount = charSet.zip(charCnt).toMap
+  private def isIdentical(word: String) = {
+    word.toLowerCase == anagram.toLowerCase
+  }
 }
