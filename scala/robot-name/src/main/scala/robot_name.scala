f95cbc16f56e5535acd05660c22ebfc347b23415
haya14busa <hayabusa1419@gmail.com>
2014-09-18 19:45:27 +0900

    scala: solve robot name

---
+++ scala/robot-name/src/main/scala/robot_name.scala
@@ -0,0 +1,22 @@
+class Robot {
+  private val oldNames = scala.collection.mutable.ListBuffer.empty[String]
+
+  def reset(): String = {
+    this.name = generateName()
+    this.name
+  }
+  var name = generateName()
+
+  private def generateName(): String = {
+    val r = new scala.util.Random
+    val prefix = r.shuffle(('A' to 'X').toList).take(2).mkString
+    val digits = r.shuffle((0 to 9).toList).take(3).mkString
+    val newName = prefix + digits
+    if (this.oldNames.find(_ == newName).isDefined)
+      this.generateName()
+    else {
+      oldNames += newName
+      newName
+    }
+  }
+}
