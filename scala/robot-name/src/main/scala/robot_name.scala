a5bd0da82557c7d831b911735239357329ac4527
haya14busa <hayabusa1419@gmail.com>
2014-09-18 19:51:41 +0900

    scala: improve variables name

--- scala/robot-name/src/main/scala/robot_name.scala
+++ scala/robot-name/src/main/scala/robot_name.scala
@@ -1,5 +1,5 @@
 class Robot {
-  private val oldNames = scala.collection.mutable.ListBuffer.empty[String]
+  private val usednames = scala.collection.mutable.ListBuffer.empty[String]
 
   def reset(): String = {
     this.name = generateName()
@@ -12,10 +12,10 @@ class Robot {
     val prefix = r.shuffle(('A' to 'X').toList).take(2).mkString
     val digits = r.shuffle((0 to 9).toList).take(3).mkString
     val newName = prefix + digits
-    if (this.oldNames contains newName)
+    if (this.usednames contains newName)
       this.generateName()
     else {
-      oldNames += newName
+      usednames += newName
       newName
     }
   }
